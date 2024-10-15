87180593a0f4634313daab2675ce1fa57ee933b3
Paula <paulacamargos26@gmail.com>
2024-10-14 23:46:27 -0300

    iniciando implementacao

---
+++ Implementação/sistemademoeda/src/main/java/com/apisistemademoeda/sistemademoeda/SistemademoedaApplication.java
@@ -0,0 +1,13 @@
+package com.apisistemademoeda.sistemademoeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemademoedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemademoedaApplication.class, args);
+	}
+
+}
---
+++ Implementação/sistemademoeda/src/test/java/com/apisistemademoeda/sistemademoeda/SistemademoedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apisistemademoeda.sistemademoeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemademoedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
