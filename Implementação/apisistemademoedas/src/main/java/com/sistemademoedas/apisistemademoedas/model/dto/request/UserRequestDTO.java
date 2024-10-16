a9158dbd5c9b852071e9f9fb88d6e68f5ebe37db
Paula <paulacamargos26@gmail.com>
2024-10-15 22:35:31 -0300

    correcao e add novos request

--- Implementação/apisistemademoedas/src/main/java/com/sistemademoedas/apisistemademoedas/model/dto/request/UserRequestDTO.java
+++ Implementação/apisistemademoedas/src/main/java/com/sistemademoedas/apisistemademoedas/model/dto/request/UserRequestDTO.java
@@ -1,8 +1,9 @@
 package com.sistemademoedas.apisistemademoedas.model.dto.request;
 
 import com.sistemademoedas.apisistemademoedas.model.enums.RoleEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record UserRequestDTO(String email,
-                              RoleEnum role,
-                              String nome,
-                              Long id) {}
+public record UserRequestDTO(@NotBlank String email,
+                             @NotNull RoleEnum role,
+                             @NotBlank  String nome) {}
