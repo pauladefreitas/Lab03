903e8dc894670ddab2922c556f27f4031d405d27
larisilvapedrosa <laripedrosasilva@gmail.com>
2024-10-22 21:48:24 -0300

    Ajustes de erros

--- Implementação/apisistemademoedas/src/main/java/com/sistemademoedas/apisistemademoedas/configuration/WebConfig.java
+++ Implementação/apisistemademoedas/src/main/java/com/sistemademoedas/apisistemademoedas/configuration/WebConfig.java
@@ -5,12 +5,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static org.springframework.web.cors.CorsConfiguration.ALL;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods(ALL)
+                .allowedHeaders(ALL)
+                .allowCredentials(true)
+                .allowedOriginPatterns(ALL);
     }
 
 }
