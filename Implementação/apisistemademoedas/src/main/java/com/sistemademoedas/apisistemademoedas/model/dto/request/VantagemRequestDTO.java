6719a05ed086da0210f4b6fdce6b87915e6d5ab7
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2024-10-30 20:13:22 -0300

    add valor

--- Implementação/apisistemademoedas/src/main/java/com/sistemademoedas/apisistemademoedas/model/dto/request/VantagemRequestDTO.java
+++ Implementação/apisistemademoedas/src/main/java/com/sistemademoedas/apisistemademoedas/model/dto/request/VantagemRequestDTO.java
@@ -3,4 +3,5 @@ package com.sistemademoedas.apisistemademoedas.model.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record VantagemRequestDTO(@NotBlank String nome,
+                                 @NotNull int valor,
                                  @NotBlank String descricao) { }
